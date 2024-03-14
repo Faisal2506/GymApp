edd733defaed8e9978b873b0931cffb9ead2a0d9
Faisal2506 <shahfaiz3105@gmail.com>
2024-03-14 15:44:27 +0530

    Add Diet Screen

--- src/screens/Diet/DietStyle.js
+++ src/screens/Diet/DietStyle.js
@@ -5,5 +5,19 @@ export const styles = StyleSheet.create({
   mainContainer:{
     flex:1,
     backgroundColor:"#000000"
+  },
+  dietDetails:{
+    color:"#fff",
+    fontFamily:"NotoSansExtraBold",
+    marginLeft:SCREEN_WIDTH/30,
+    marginBottom:SCREEN_HEIGHT/40
+  },
+  dietInfo:{
+    // borderWidth:1,borderColor:"#fff",
+    marginTop:SCREEN_HEIGHT/20,
+    paddingHorizontal:SCREEN_WIDTH/40
+  },
+  detailsContainer:{
+    flexDirection:"row"
   }
 })
