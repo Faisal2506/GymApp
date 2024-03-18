a04e1f807608c4b5d01a1bfc8db136775a22b4e5
Faisal2506 <shahfaiz3105@gmail.com>
2024-03-18 12:53:06 +0530

    Add Membership detail page

--- src/screens/MembershipDetails/MembershipDetailsStyle.js
+++ src/screens/MembershipDetails/MembershipDetailsStyle.js
@@ -4,22 +4,42 @@ import { SCREEN_HEIGHT,SCREEN_WIDTH,getResponsiveFontSize } from '../../constant
 export const styles = StyleSheet.create({
   mainContainer:{
     flex:1,
-    backgroundColor:"#000000"
+    backgroundColor:"#000000",
+    paddingHorizontal:SCREEN_WIDTH/40
+  },
+  detailsTitle:{
+    color:"#F37293",
+    fontSize:getResponsiveFontSize(18),
+    marginLeft:SCREEN_WIDTH/30,
+    // borderWidth:1,borderColor:"#fff",
+    marginVertical:SCREEN_HEIGHT/50,
+    fontFamily:"NotoSansExtraBold",
   },
   details:{
-    borderWidth:1,borderColor:"#fff",
+    // borderWidth:1,borderColor:"#999",
     paddingHorizontal:SCREEN_WIDTH/30,
     borderRadius:20,
     height:SCREEN_HEIGHT/3,
-    justifyContent:"space-evenly"
+    justifyContent:"space-evenly",
+    backgroundColor:"#999"
   },
   container:{
     borderBottomWidth:1,
     borderColor:"#fff",
-    // paddingBottom:SCREEN_HEIGHT/30,
+    // paddingBottom:SCREEN_HEIGHT/50,
     justifyContent:"center",
+    height:SCREEN_HEIGHT/20
   },
   detailsText:{
-    color:"#fff"
+    color:"#fff",
+    marginLeft:SCREEN_WIDTH/30,
+    fontFamily:"NotoSansExtraBold",
+    color:"#D0FD3E"
+  },
+  memberInfo:{
+    height:SCREEN_HEIGHT/2
+  },
+  payment:{
+    height:SCREEN_HEIGHT/1.2
   }
 })
