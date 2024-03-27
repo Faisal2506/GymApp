026ca92f54209ddedc392b2aa7f4f4659f01f5be
Faisal2506 <shahfaiz3105@gmail.com>
2024-03-27 15:22:56 +0530

    Invitation Screen Update

--- src/screens/Invitation/InvitationScreen.js
+++ src/screens/Invitation/InvitationScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import FocusAwareStatusBar from '../../components/StatusBar/FocusAwareStatusBar'
 import { styles } from './InvitationStyle'
@@ -7,15 +7,26 @@ const InvitationScreen = () => {
   return (
     <View style={styles.mainContainer}>
       <FocusAwareStatusBar isLightBar={true} isTopSpace={true} isTransparent={true} />
-      <Text style={styles.invitationDetails}>ID</Text>
-      <Text style={styles.invitationDetails}>Type</Text>
+      <Image source={require("../../../assets/images/Invitation/invitation01.jpg")} style={styles.imageStyle} />
+      <View style={styles.allDetailsContainer}>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>ID</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>Type</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>Title</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>Description</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>Date</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>From Time</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>To Time</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>Status</Text></View>
+      <View style={styles.detailesContainer}><Text style={styles.invitationDetails}>Created Date</Text></View>
+      </View>
+      {/* <Text style={styles.invitationDetails}>Type</Text>
       <Text style={styles.invitationDetails}>Title</Text>
       <Text style={styles.invitationDetails}>Description</Text>
       <Text style={styles.invitationDetails}>Date</Text>
       <Text style={styles.invitationDetails}>From Time</Text>
       <Text style={styles.invitationDetails}>To Time</Text>
       <Text style={styles.invitationDetails}>Status</Text>
-      <Text style={styles.invitationDetails}>Created Date</Text>
+      <Text style={styles.invitationDetails}>Created Date</Text> */}
     </View>
   )
 }
