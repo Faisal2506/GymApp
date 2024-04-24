0e5e907d6c8e8c539d1ed0543e18b2ec5ef8e53b
Faisal2506 <shahfaiz3105@gmail.com>
2024-04-24 15:58:22 +0530

    Freeze page theme done

--- src/screens/FreezeListing/FreezeListing.js
+++ src/screens/FreezeListing/FreezeListing.js
@@ -4,8 +4,10 @@ import FocusAwareStatusBar from '../../components/StatusBar/FocusAwareStatusBar'
 import { styles } from './FreezeListingStyle'
 import Button from '../../components/Button/Button'
 import ConfirmModal from '../../components/ConfirmModal/ConfirmModal'
+import { useTheme } from '../../context/ThemeContext'
 
 const FreezeListing = ({navigation}) => {
+  const { theme, isDarkMode } = useTheme();
   const [confirmModalVisible,setConfirmModalVisible] = useState(false);
   const [freeze,setFreeze] = useState(false);
 
@@ -19,17 +21,17 @@ const FreezeListing = ({navigation}) => {
     console.log(freeze)
   }
   return (
-    <View style={styles.mainContainer}>
-      <FocusAwareStatusBar isLightBar={true} isTopSpace={true} isTransparent={true} />
+    <View style={[styles.mainContainer,{backgroundColor: theme.backgroundColor}]}>
+      <FocusAwareStatusBar isLightBar={isDarkMode?true:false} isTopSpace={true} isTransparent={true} />
       <Button title={"Freeze"}  buttonFunction={freezeHandle} buttonStyle={freeze?styles.activeStyle:styles.inactiveStyle}/>
       <Button title={"Unfreeze"} buttonFunction={freezeHandle} buttonStyle={freeze?styles.inactiveStyle:styles.activeStyle}/>
       <ConfirmModal modalVisible={confirmModalVisible} toggleModal={freezeHandle} yesHandle={freezeYesHandle}/>
       <View>
-        <Text style={styles.details}>{freeze}</Text>
-        <Text style={styles.details}>ID</Text>
-        <Text style={styles.details}>Type</Text>
-        <Text style={styles.details}>Date</Text>
-        <Text  style={styles.details}>Created Date</Text>
+        <Text style={[styles.details,{color: theme.greenText}]}>{freeze}</Text>
+        <Text style={[styles.details,{color: theme.greenText}]}>ID</Text>
+        <Text style={[styles.details,{color: theme.greenText}]}>Type</Text>
+        <Text style={[styles.details,{color: theme.greenText}]}>Date</Text>
+        <Text style={[styles.details,{color: theme.greenText}]}>Created Date</Text>
       </View>
     </View>
   )
